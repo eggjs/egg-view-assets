6aa47a4a729a8d424f261fe3ba4c44e37f1047e1
sky <cl98914@alibaba-inc.com>
2020-08-24 09:59:10 +0800

    style: not need new line

--- app.js
+++ app.js
@@ -30,10 +30,7 @@ module.exports = app => {
 
   // it should check manifest.json on deployment
   if (!assetsConfig.isLocalOrUnittest) {
-    const manifestPath = path.join(
-      app.config.baseDir,
-      'config/manifest.json'
-    );
+    const manifestPath = path.join(app.config.baseDir, 'config/manifest.json');
     assert(fs.existsSync(manifestPath), `${manifestPath} is required`);
     assetsConfig.manifest = require(manifestPath);
   }
